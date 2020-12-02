e63686803c3aa24d8968ecd6f02bdeefe5e8c830
Pat Senechal <patsen29@gmail.com>
2020-12-01 23:47:17 -0400

    Solving second half of day 1

--- src/main/java/advent/Day01.java
+++ src/main/java/advent/Day01.java
@@ -12,17 +12,39 @@ public class Day01 {
         List<Integer> lines = Files.readAllLines(Paths.get("src/main/resources/day01.txt")).stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        System.out.println("Part 1: " + findProduct(lines, 2020));
+        System.out.println("Part 1: " + findProductOfTwo(lines, 2020));
+        System.out.println("Part 2: " + findProductOfThree(lines, 2020));
     }
 
-    /** Logic for first half of the puzzle. Builds sorted set, looking for complement  */
-    static int findProduct(List<Integer> list, int target) {
-        SortedSet<Integer> sortedSet = new TreeSet<>();
+    /** Logic to find a given pair in a set with a given target sum.
+     * @return the product of the two numbers, or 0 if none were found. */
+    private static int findPairWithSum(Set<Integer> set, int target) {
+        if (target <= 0) return 0;
+        // TODO: Only loop over numbers smaller than half?
+        for (int first : set) {
+            int second = target - first;
+            if (set.contains(second)) {
+                return first * second;
+            }
+        }
+        return 0;
+    }
+
+    /** Function to find a product of two numbers in a list with a given sum. */
+    static int findProductOfTwo(List<Integer> list, int target) {
+        Set<Integer> sorted = new TreeSet<>(list);
+        return findPairWithSum(sorted, target);
+    }
+
+    /** Function to find a product of three numbers in a list with a given sum. */
+    static int findProductOfThree(List<Integer> list, int target) {
+        Set<Integer> sortedSet = new TreeSet<>();
         for (int num : list) {
-            int lookingFor = target - num;
-            if (sortedSet.contains(lookingFor)) {
-                // println("Found match $num and $lookingFor")
-                return num * lookingFor;
+            int remaining = target - num;
+            int potentialProduct = findPairWithSum(sortedSet, remaining);
+            if (potentialProduct > 0) {
+                // println("Found triple: $num, ${potentialPair.first}, ${potentialPair.second}")
+                return num * potentialProduct;
             }
             sortedSet.add(num);
         }
--- src/test/java/advent/Day01Test.java
+++ src/test/java/advent/Day01Test.java
@@ -11,6 +11,7 @@ public class Day01Test {
     @Test
     public void testFindProduct() {
         List<Integer> example = Arrays.asList(1721, 979, 366, 299, 675, 1456);
-        assertEquals(514579, Day01.findProduct(example, 2020));
+        assertEquals(514579, Day01.findProductOfTwo(example, 2020));
+        assertEquals(241861950, Day01.findProductOfThree(example, 2020));
     }
 }
