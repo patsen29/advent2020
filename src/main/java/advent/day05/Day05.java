8081259ece00e7eab1539bb2df38162c54f3e10c
Pat Senechal <patsen29@gmail.com>
2020-12-05 18:16:19 -0400

    Day 05: Second half, get rid of BoardingPass class

--- src/main/java/advent/day05/Day05.java
+++ src/main/java/advent/day05/Day05.java
@@ -3,18 +3,22 @@ package advent.day05;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Day05 {
     public static void main(String[] args) throws IOException {
-        List<BoardingPass> passes = Files.readAllLines(Paths.get("src/main/resources/day05.txt")).stream()
+        Set<Integer> passes = Files.readAllLines(Paths.get("src/main/resources/day05.txt")).stream()
                 .map(Day05::parsePass)
-                .collect(Collectors.toList());
-        System.out.println("Part 1: " + findHighestPass(passes));
+                .collect(Collectors.toSet());
+
+        int max = findHighestPass(passes);
+        System.out.println("Part 1: " + max);
+        System.out.println("Part 2: " + findMissingPass(passes, max));
     }
 
-    public static BoardingPass parsePass(String code) {
+    public static int parsePass(String code) {
         int id = 0;
         for (int i = 0; i < code.length(); i++) {
             id *= 2;
@@ -23,15 +27,21 @@ public class Day05 {
                 id++;
             }
         }
-        int row = id / 8;
-        int col = id % 8;
-        return new BoardingPass(row, col);
+        return id;
     }
 
-    public static int findHighestPass(List<BoardingPass> passes) {
+    public static int findHighestPass(Collection<Integer> passes) {
         return passes.stream()
-                .max(BoardingPass::compareTo)
-                .map(BoardingPass::getSeatId)
+                .max(Integer::compareTo)
                 .orElse(-1);
+    }
+
+    public static int findMissingPass(Set<Integer> passes, int max) {
+        for (int i = 1; i < max; i++) {
+            if (passes.contains(i - 1) && !passes.contains(i) && passes.contains(i + 1)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("Could not find gap on plane");
     }
 }
--- src/test/java/advent/Day05Test.java
+++ src/test/java/advent/Day05Test.java
@@ -1,6 +1,5 @@
 package advent;
 
-import advent.day05.BoardingPass;
 import advent.day05.Day05;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day05Test {
     @Test
     public void testParsing() {
-        BoardingPass one = Day05.parsePass("BFFFBBFRRR");
-        assertEquals(70, one.row);
-        assertEquals(7, one.column);
-        assertEquals(567, one.getSeatId());
-
-        assertEquals(119, Day05.parsePass("FFFBBBFRRR").getSeatId());
-        assertEquals(820, Day05.parsePass("BBFFBBFRLL").getSeatId());
+        assertEquals(567, Day05.parsePass("BFFFBBFRRR"));
+        assertEquals(119, Day05.parsePass("FFFBBBFRRR"));
+        assertEquals(820, Day05.parsePass("BBFFBBFRLL"));
     }
 }
